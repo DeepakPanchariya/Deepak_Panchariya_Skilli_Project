f814c48b278e33c7e2617fcc3e75136b64c06670
Deepak_Panchariya <69053339+HackOHolicks@users.noreply.github.com>
2021-10-22 03:35:12 +0530

    Landscape mode implemented share and add camera image done

--- app/src/main/java/com/testproject/deepak_panchariya_skilli_project/FollowedByActivity.kt
+++ app/src/main/java/com/testproject/deepak_panchariya_skilli_project/FollowedByActivity.kt
@@ -1,25 +1,41 @@
 package com.testproject.deepak_panchariya_skilli_project
 
+import android.content.ContentValues
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.squareup.picasso.Picasso
+import java.io.ByteArrayOutputStream
+import java.util.*
 
 class FollowedByActivity : AppCompatActivity() {
     lateinit var userImage:ImageView
     lateinit var titleText:TextView
     lateinit var publishAtText:TextView
     lateinit var commentText:TextView
+    lateinit var clickImage: FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_followed_by)
         intiValues()
+
+        clickImage.setOnClickListener {
+           shareImage()
+        }
     }
 
     private fun intiValues() {
-
+        clickImage=findViewById(R.id.clickPhoto)
         userImage=findViewById(R.id.userImageImgView)
         titleText=findViewById(R.id.nameTitleTV)
         publishAtText=findViewById(R.id.publishedAtTV)
@@ -33,5 +49,32 @@ class FollowedByActivity : AppCompatActivity() {
         titleText.text=intent.getStringExtra("title")
         publishAtText.text=intent.getStringExtra("publishAt")
         commentText.text=intent.getStringExtra("comment")
+    }
+    private fun shareImage() {
+
+        val bMap = (userImage.drawable as BitmapDrawable).bitmap
+        val uriImage=getImageUri(this,bMap)
+       // val imageAddress=intent.getStringExtra("picture")
+        val share = Intent(Intent.ACTION_SEND)
+      //  val imageUri = Uri.parse(imageAddress.toString())
+        share.type = "image/jpeg"
+
+        val values = ContentValues()
+        values.put(MediaStore.Images.Media.TITLE, "IMG_" + Calendar.getInstance().time)
+        values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
+        share.putExtra(Intent.EXTRA_STREAM, uriImage)
+        startActivity(Intent.createChooser(share, "Share Image"))
+    }
+    fun getImageUri(inContext: Context, inImage: Bitmap): Uri? {
+        val bytes = ByteArrayOutputStream()
+        val currentDate=Date(System.currentTimeMillis())
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes)
+        val path = MediaStore.Images.Media.insertImage(
+            inContext.contentResolver,
+            inImage,
+            "IMG_$currentDate",
+            null
+        )
+        return Uri.parse(path)
     }
 }
--- app/src/main/java/com/testproject/deepak_panchariya_skilli_project/MainActivity.kt
+++ app/src/main/java/com/testproject/deepak_panchariya_skilli_project/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.testproject.deepak_panchariya_skilli_project
 
+import android.Manifest
 import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.gson.JsonArray
 import com.google.gson.JsonObject
 import com.testproject.deepak_panchariya_skilli_project.Adaptor.RecyclerAdaptor
@@ -15,8 +23,15 @@ import org.json.JSONArray
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.io.ByteArrayOutputStream
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
+    lateinit var adaptercalling:RecyclerAdaptor
+    private val CAMERA_REQUEST = 1888
+    val imageCaptureCode = 2005
+    lateinit var clickImage: FloatingActionButton
     lateinit var recyclerView: RecyclerView
     var imageArrayList:ArrayList<DiscriptionList> = ArrayList()
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,9 +39,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         initValues()
 
+        clickImage.setOnClickListener {
+            dispatchTakePictureIntent()
+        }
+
     }
 
     private fun initValues() {
+        clickImage=findViewById(R.id.clickPhoto)
         recyclerView=findViewById(R.id.recyclerView)
         val llm = LinearLayoutManager(this)
         llm.orientation = LinearLayoutManager.VERTICAL
@@ -35,15 +55,21 @@ class MainActivity : AppCompatActivity() {
 
 
     }
+    private fun dispatchTakePictureIntent() {
+        if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+
+            requestPermissions(arrayOf(Manifest.permission.CAMERA), CAMERA_REQUEST)
+        } else {
+            val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(cameraIntent, imageCaptureCode)
+
+        }
+    }
     private fun retrofitgamedata(){
         imageArrayList.clear()
 
         val jsonvalues= JsonObject()
-
-        jsonvalues.addProperty("env_type", "none")
-       // jsonvalues.addProperty("unique_token", session.getSession_userid())
-
-        //retrofit client calling
+        jsonvalues.addProperty("none", "none")
         Log.e("Sending",jsonvalues.toString())
         RetrofitClient.service.userGetData(jsonvalues).enqueue(
             object : Callback<JsonArray> {
@@ -95,7 +121,7 @@ class MainActivity : AppCompatActivity() {
 //
 //
 //
-                    val adaptercalling = RecyclerAdaptor(
+                    adaptercalling = RecyclerAdaptor(
                         applicationContext,
                         imageArrayList
                     )
@@ -130,5 +156,39 @@ class MainActivity : AppCompatActivity() {
 
 
 
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == imageCaptureCode && resultCode == RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            val imagePath=getImageUri(this,imageBitmap)
+            imageArrayList.add(0,
+                DiscriptionList("User image added from camera",imagePath.toString(),"124578",Date(System.currentTimeMillis()).toString(),"Camera Added")
+            )
+            adaptercalling.notifyDataSetChanged()
+//            val adaptercalling = RecyclerAdaptor(
+//                applicationContext,
+//                imageArrayList
+//            )
+//            recyclerView.setHasFixedSize(true)
+//            adaptercalling.notifyDataSetChanged()
+//            recyclerView.adapter = adaptercalling
+
+
+
+        }
+    }
+    fun getImageUri(inContext: Context, inImage: Bitmap): Uri? {
+        val bytes = ByteArrayOutputStream()
+        val currentDate=Date(System.currentTimeMillis())
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes)
+        val path = MediaStore.Images.Media.insertImage(
+            inContext.contentResolver,
+            inImage,
+            "IMG_$currentDate",
+            null
+        )
+        return Uri.parse(path)
     }
 }
